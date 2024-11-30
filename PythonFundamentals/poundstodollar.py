0ecab308dbb2b6e36e158f66281526ee426dbc0c
Karun Binoy <127982160+karun84@users.noreply.github.com>
2024-11-30 21:09:56 +0530

    Add files via upload

---
+++ PythonFundamentals/poundstodollar.py
@@ -0,0 +1,2 @@
+sum=int(input("please enter amounts in pounds: "))
+print(f"£{sum} are ${sum}")
---
+++ PythonFundamentals/pythonFundamentals.py
@@ -0,0 +1,57 @@
+#1st question
+print("Karun","dsml41","karunbinoy@gmail.com")
+
+#2nd question
+print("Karun","\ndsml41","\nkarunbinoy@gmail.com")
+
+#3rd question
+a=14
+b=7
+print(f"{a} + {b} = {a+b}")
+print(f"{a} * {b} = {a+b}")
+print(f"{a} - {b} = {a+b}")
+print(f"{a} / {b} = {a+b}")
+
+#4th question
+for i in range(1,6):
+    print(i)
+
+#5th question
+print('"SDK" stands for "Software Development Kit", whereas\n"IDE" stands for "Integrated Development Environment".')
+
+#6th question
+print("python is an \"awesome\" language.")
+print("python\n\t2023")
+print('I\'m from Entri.\b')
+print("\65")
+print("\x65")
+print("Entri", "2023", sep="\n")
+print("Entri", "2023", sep="\b")
+print("Entri", "2023", sep="*", end="\b\b\b\b")
+
+#7th question
+num=23
+textnum="57"
+decimal=98.3
+
+print(type(num))
+print(type(textnum))
+print(type(decimal))
+
+sum=num + int(textnum) + decimal
+print(sum)
+print(type(sum))
+
+#8th question
+no_of_days_in_year=365
+no_of_minutes_in_hour=60
+no_of_hours_in_day=24
+
+total_noof_minutes_ina_year=no_of_days_in_year*no_of_hours_in_day*no_of_minutes_in_hour
+print(total_noof_minutes_ina_year)
+
+#9th question
+name=input("please enter your name: ")
+print(f"Hello, {name}!, Welcome to python programming")
+
+
